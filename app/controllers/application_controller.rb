6c0a86241a35c2dee83b6dbe2e07100b76793a71
Nazrul <nazrulku07@gmail.com>
2018-12-14 14:26:00 +0600

    more links add in devise form and notice add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  add_flash_types :success, :warning, :danger, :info
 
   def after_sign_in_path_for(resource)
     blogs_path
